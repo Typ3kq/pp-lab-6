80253df484a19a5c6ea51308b2dec45729a8fa77
Typ3kq <kacpertywonek60@gmail.comm>
2024-05-05 09:27:03 +0200

    zad3

--- Main.java
+++ Main.java
@@ -1,30 +1,20 @@
-import java.util.ArrayList;
 import company.models.Manager;
 import company.models.Worker;
-import company.abstracts.Employee;
 
 public class Main {
     public static void main(String[] args) {
         Worker worker1 = new Worker("Roberto", 3000, 1, "2023-09-01", "Praktykant");
         Worker worker2 = new Worker("Lily", 1500, 2, "2022-04-10", "Specjalista");
-        Worker worker3 = new Worker("Jamie", 1800, 3, "2021-11-11", "Kierownik");
-        Worker worker4 = new Worker("Mike", 2100, 4, "2020-05-05", "Dyrektor");
+        Worker worker3 = new Worker("Jamie", 1800, 1, "2021-11-11", "Kierownik"); 
+        Manager manager = new Manager("George", 7000, 3, "2018-02-20", "Prezes");
 
-        Manager manager = new Manager("George", 7000, 5, "2018-02-20", "Prezes");
+        System.out.println(worker1.getName() + " has code: " + worker1.hashCode());
+        System.out.println(worker2.getName() + " has code: " + worker2.hashCode());
+        System.out.println(worker3.getName() + " has code: " + worker3.hashCode());
+        System.out.println(manager.getName() + " has code: " + manager.hashCode());
 
-        ArrayList<Employee> employees = new ArrayList<>();
-        employees.add(worker1);
-        employees.add(worker2);
-        employees.add(worker3);
-        employees.add(worker4);
-        employees.add(manager);
-
-        for (Employee emp : employees) {
-            System.out.println("- " + emp.getName() + " (ID: " + emp.getId() +
-                    ", Position: " + emp.getPosition() +
-                    ", Hire date: " + emp.getHireDate() +
-                    ", Salary: " + emp.getSalary() + ")");
-            emp.work();
-        }
+        System.out.println(worker3.getName() + " equals worker1: " + worker3.equals(worker1));
+        System.out.println(worker3.getName() + " equals worker2: " + worker3.equals(worker2));
+        System.out.println(worker3.getName() + " equals manager: " + worker3.equals(manager));
     }
 }
